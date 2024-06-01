c512e54b74d40b3f85359121f395790d47fbf114
Jack Leightcap <jack@leightcap.com>
2024-06-01 14:00:49 -0400

    iterator, randomized tests
    Signed-off-by: Jack Leightcap <jack@leightcap.com>

--- test/test.py
+++ test/test.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+import random
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
@@ -30,18 +31,15 @@ async def phase_alternate(dut):
     await ClockCycles(dut.clk, 1)
     assert dut.count.value == 0
     assert dut.we == 0
-    await ClockCycles(dut.clk, 1)
-    assert dut.count.value == 1
-    assert dut.we != 0
 
 @cocotb.test()
 async def signed_unsigned_addition(dut):
     await cocotb.start(setup(dut))
 
-    dut.fraction.value = 19
-    dut.accumulator.value = 38
+    dut.fraction.value = frac = random.randint(0, 50)
+    dut.accumulator.value = acc = random.randint(0,50)
     await ClockCycles(dut.clk, 2)
-    assert dut.degree.value == 19 + 38
+    assert dut.degree.value == frac + acc
 
 @cocotb.test()
 async def fraction_exhausted(dut):
@@ -49,8 +47,20 @@ async def fraction_exhausted(dut):
     fraction iterator consumed before accumulator iterator
     """
 
-    await cocotb.start(setup(dut))
+    await setup(dut)
 
     dut.fraction.value = 1
     dut.accumulator.value = 2
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 2)
+    assert dut.degree.value == 3
+
+    # fraction terminates
+    dut.fraction.value = 0b11111111
+    dut.accumulator.value = 10
+    await ClockCycles(dut.clk, 2)
+    assert dut.degree.value == 10
+
+    # accumulator also terminates, output terminated
+    dut.accumulator.value = 0b11111111
+    await ClockCycles(dut.clk, 2)
+    assert dut.degree.value == 0b11111111
